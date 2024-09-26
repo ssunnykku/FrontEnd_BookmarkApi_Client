2ebc3c1f7ebb9ec5d5bfd78afad61cd9e14db7e7
ssunnykku <ssunnykku@gmail.com>
2024-09-26 13:41:04 +0900

    axios add & bookmarkApi add

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -1,9 +1,17 @@
-import "bootstrap/dist/css/bootstrap.min.css";
+import { getAllBookmarks } from "@/lib/bookmarkApi";
+import { BookmarkType, BookmarksResponse } from "@/types";
 
-export default function Home() {
+export default async function BookmarkPage() {
+  const bookmarksData: Promise<BookmarksResponse> = getAllBookmarks();
+  const { data } = await bookmarksData;
+  const bookmarks: BookmarkType[] = data;
+
   return (
     <div>
       <h1>Welcome to Bookmarker</h1>
+      {bookmarks.map((bookmark) => {
+        return <h2 key={bookmark.id}>{bookmark.title}</h2>;
+      })}
     </div>
   );
 }
