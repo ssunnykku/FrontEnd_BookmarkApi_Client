763fecf984ebd2f6957a533106b926e301953f5e
ssunnykku <ssunnykku@gmail.com>
2024-09-26 14:22:38 +0900

    bookmark component 작성

--- src/app/components/Bookmarks.tsx
+++ src/app/components/Bookmarks.tsx
@@ -1,5 +1,6 @@
 import { BookmarkType } from "@/types";
 import React from "react";
+import Bookmark from "./Bookmark";
 
 type Props = {
   bookmarks: BookmarkType[];
@@ -9,9 +10,9 @@ const Bookmarks: React.FC<Props> = ({ bookmarks }) => {
   return (
     <div>
       <h1>Welcome to Bookmarker</h1>
-      {bookmarks.map((bookmark) => {
-        return <h3 key={bookmark.id}>{bookmark.title}</h3>;
-      })}
+      {bookmarks.map((bookmark) => (
+        <Bookmark key={bookmark.id} bookmark={bookmark} />
+      ))}
     </div>
   );
 };
